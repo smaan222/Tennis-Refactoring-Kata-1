ce9e18f5c13e98cc9b50ee25ad06f2ab29aa96f3
Shounka Maan <shounka.maan@gmail.com>
2020-08-04 09:26:25 -0400

    Update TennisGame.cs
    i created new methods

--- csharp/Tennis/TennisGame.cs
+++ csharp/Tennis/TennisGame.cs
@@ -1,6 +1,7 @@
 namespace Tennis
 {
-    class TennisGame : ITennisGame
+    // changes class to public
+    public class TennisGame : ITennisGame
     {
         private int m_score1 = 0;
         private int m_score2 = 0;
@@ -46,34 +47,63 @@ namespace Tennis
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = DetermineWinner(m_score1, m_score2);
             }
             else
             {
-                for (var i = 1; i < 3; i++)
+                CalculateScore(ref score, ref tempScore);
+            }
+            return score;
+        }
+        /// <summary>
+        /// /
+        /// </summary>
+        /// <param name="score"></param>
+        /// <param name="tempScore"></param>
+       
+        //created new method called clculate score
+        private void CalculateScore(ref string score, ref int tempScore)
+        {
+            for (var i = 1; i < 3; i++)
+            {
+                if (i == 1) tempScore = m_score1;
+                else { score += "-"; tempScore = m_score2; }
+                switch (tempScore)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
                 }
+            }
+        }
+        //created new meothed determine the winner and made code more readable
+        private string DetermineWinner(int score1, int score2)
+        {
+            string score;
+            var minusResult = score1 - score2;
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            }
+            else if (minusResult == -1)
+            {
+                score = "Advantage player2";
+            }
+            else if(minusResult >= 2)
+            {
+                score = "Win for player1";
+            }
+            else 
+            { 
+                score = "Win for player2";
             }
             return score;
         }
